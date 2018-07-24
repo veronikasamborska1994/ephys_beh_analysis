0660c9adbad8caea169873ad91b9850791113bcc
BehrensLab <v.sambor@gmail.com>
2018-07-24 15:10:56 +0100

    position code changes

--- position_code.py
+++ position_code.py
@@ -28,6 +28,12 @@ def uncycle(time):
     cycleindex = np.cumsum(cycles)
     return time + cycleindex * 128
 
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx]
+
+
 ephys_session = fu.load_data('m486_2018-07-23_19-25-22','/media/behrenslab/90c50efc-05cf-4045-95e4-9dabd129fb47/Ephys_Reversal_Learning/data/Ephys 3 Tasks Processed Spikes/m486/openfield','/',True)
 clusters = ephys_session['spike_cluster'].unique()
 xy = np.loadtxt(open('/media/behrenslab/90c50efc-05cf-4045-95e4-9dabd129fb47/m486_open_fielxyz/openfield_m486_xy2018-07-23T19_25_14.csv'))
@@ -35,18 +41,18 @@ time_camera = np.loadtxt(open('/media/behrenslab/90c50efc-05cf-4045-95e4-9dabd12
 time_camera = np.asarray(time_camera,dtype=uint32)
 converted_time = converttime(time_camera)
 uncycled_time = uncycle(converted_time)
+uncycled_time =uncycled_time*1000
 
-bin_edges_trial = np.arange(0 ,100, 50)
-bin_width_ms = 50 
-smooth_sd_ms = 100
 
 for i,cluster in enumerate(clusters): 
     spikes_to_plot = []
+    spikes_per_frame_list =[]
+    
     spikes = ephys_session.loc[ephys_session['spike_cluster'] == 50]
     spikes_times = np.array(spikes['time'])
+    spikes_times = spikes_times[~np.isnan(spikes_times)]
     for c,coordinate in enumerate(xy):
-        time_at_xy = time_camera[c]
-        spikes_ind = spikes_times[(spikes_times >= -100) & (spikes_times<= +100)]        
-        spikes_to_plot.append(spikes_ind) 
-        hist,edges = np.histogram(spikes_to_plot, bins= bin_edges_trial)
-        normalised = gaussian_filter1d(hist.astype(float), smooth_sd_ms/bin_width_ms)
+        time_at_xy = uncycled_time[c]
+        assign_spike_to_xy = find_nearest(spikes_times, time_at_xy )
+        spikes_per_frame_list.append(assign_spike_to_xy)
+        
